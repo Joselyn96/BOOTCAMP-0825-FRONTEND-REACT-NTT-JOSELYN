bbd6af807f6505914075abdeab2190ae82df6242
Joselyn96 <joselynvalverde6916@gmail.com>
2025-09-14 01:00:10 -0500

    refactor: se ha movido componentes de App a pages/Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,15 @@
-import { useState} from 'react'
-import "./App.css"
-// import MyAppStyled from './App.styled'
-import Navbar from './components/layout/Navbar'
-import Sidebar from './components/layout/Sidebar'
-import Hero from './components/layout/Hero'
-import Benefits from './components/layout/Benefits'
-import Categories from './components/layout/Categories'
-import Carousel from './components/layout/Carousel'
-import Newsletter from './components/layout/Newsletter'
-import Testimonial from './components/layout/Testimonial'
 
-//hooks
-import { useScroll } from './hooks/useScroll'
+import "./App.css"
+import MyAppStyled from './App.styled'
 
 function App() {
-  const isScrolled = useScroll(50)
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen)
-  }
-
-  const closeSidebar = () => {
-    setIsSidebarOpen(false)
-  }
 
 
   return (
-    <div className="min-h-screen">
-      {isSidebarOpen && <div className="sidebar-overlay" onClick={closeSidebar} />}
-      <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
-      <Navbar isScrolled={isScrolled} onToggleSidebar={toggleSidebar} />
-      <Hero />
-      <Benefits />
-      <Categories />
-      <Carousel />
-      <Newsletter />
-      <Testimonial />
-    </div>
-    // <>
-    //   <h1>pruebas</h1>
-    //   <MyAppStyled.Botoncito>Click me</MyAppStyled.Botoncito>
-    // </>
+    <>
+      <h1>pruebas</h1>
+      <MyAppStyled.Botoncito>Click me</MyAppStyled.Botoncito>
+    </>
   )
 }
 
